4f635f611a50c576f9e8c16eb0be9ef2d8e18f5b
shivangi gupta <shivangi.gupta@student.allenisd.org>
2022-10-03 13:11:41 -0500

    adding code

--- main.swift
+++ main.swift
@@ -1,1 +1,36 @@
+
+bubbleSort(unsortedIntegers)
+func swap(integers: inout [Int], firstIndex: Int, secondIndex: Int){
+let temp =  integers[firstIndex]
+integers[firstIndex] = integers[secondIndex]
+integers[secondIndex] = temp
+}
+func bubbleSort(_ unsortedIntegers: [Int])  {
+    var sortingIntegers = unsortedIntegers
+
+    var totalSwapCount = 0
+    var  swapCountPerPass: Int
+    var x  = 0
+    print("Pass: 0, Swaps: 0/0, Array: \(unsortedIntegers)")
+    repeat {
+        x += 1
+        swapCountPerPass = 0
+        for index in 0 ..< sortingIntegers.count - 1{
+            let thisIndex = index
+            let nextIndex = index + 1
+
+            let thisElement = sortingIntegers[thisIndex]
+            let nextElement =  sortingIntegers[nextIndex]
+            if thisElement > nextElement {
+                swap(integers: &sortingIntegers, firstIndex: thisIndex, secondIndex: nextIndex)
+            totalSwapCount += 1
+            swapCountPerPass +=  1
+
+            }
+        }
+        print("Pass: \(x), Swaps: \(swapCountPerPass)/\(totalSwapCount), Array: \(sortingIntegers)")
+   
+    }while swapCountPerPass > 0
+}
+
    
